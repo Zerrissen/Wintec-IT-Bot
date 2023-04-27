544cd735e3f97de74ae39ba8688396183b854f4a
JamesOlwyn <113389464+JamesOlwyn@users.noreply.github.com>
2023-04-28 06:51:11 +1200

    Update help.js

--- commands/fun/help.js
+++ commands/fun/help.js
@@ -1,19 +1,10 @@
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+{ SlashCommandBuilder } = require("discord.js");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('messageCreate', async message => {
-  if (message.content === '!help') {
-    const embed = new MessageEmbed()
-      .setColor('#0099ff')
-      .setTitle('Help')
-      .setDescription('This is a help message');
-
-    await message.channel.send({ embeds: [embed] });
-  }
-});
-
-client.login('YOUR_TOKEN_HERE');
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName('!Help')
+        .setDescription('Replies with Help'),
+    async execute(interaction) {
+        await interaction.reply('Hello here are the current avalible commands. ping')
+    }
+}
