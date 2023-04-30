24a39ba8dcd679133b161e83f3289a7a121dd9bd
Zerrissen <zerrissen@tutamail.com>
2023-04-30 03:51:13 +0000

    Fix Database Connection On branch v1.0.1 Changes to be committed: 	modified:   .gitignore 	modified:   src/handlers/handleDatabase.js
    Modified DB access and connection strings

--- src/handlers/handleDatabase.js
+++ src/handlers/handleDatabase.js
@@ -4,14 +4,14 @@ const path = require("path");
 module.exports = async (client) => {
     client.handleDatabase = async () => {
         try {
-            let mongoCertPath = path.resolve(process.env.MONGO_CERT_PATH);
 
             await connect(process.env.MONGO_DB, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
-                tlsCAFile: mongoCertPath,
+                dbName: "Bot-Prod"
             });
         } catch (error) {
+            console.error(error);
             connection.emit("error", error);
             process.exit(1);
         }
