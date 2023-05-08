0224bb608546b4abf940dd262ea36e04d2abc5c4
Nathan <54736633+Zerrissen@users.noreply.github.com>
2023-05-08 17:30:04 +1200

    Comment component handler On branch 8-commentdocument-code Changes to be committed: 	modified:   src/handlers/handleComponents.js
    Effective changes:
    - Commented component handler and separated relevant code blocks appropriately.

--- src/handlers/handleComponents.js
+++ src/handlers/handleComponents.js
@@ -1,7 +1,16 @@
+/*------------------ REQUIRES -----------------*/
 const { readdirSync } = require('fs');
+/*------------------ END OF REQUIRES -----------------*/
 
+/**==============================================
+ **                   INFO
+ *?  The below function gets exported and made
+ *?  Available to the client object in index.js.
+ *@return function
+ *=============================================**/
 module.exports = (client) => {
     client.handleComponents = async () => {
+        //* Grab all components and make them available to the client
         const componentFolders = readdirSync('./src/components');
         for (const folder of componentFolders) {
             const componentFiles = readdirSync(
@@ -9,6 +18,7 @@ module.exports = (client) => {
             ).filter((file) => file.endsWith('.js'));
 
             const { modals } = client;
+            // * Determine the type of component and set its properties accordingly
             switch (folder) {
                 case 'modals':
                     for (const file of componentFiles) {
