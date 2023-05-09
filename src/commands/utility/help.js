2f17573f9c935c35104e750752ebcd551c318b3f
SkulduggeryDude <131653869+SkulduggeryDude@users.noreply.github.com>
2023-05-10 10:38:55 +1200

    Update help.js

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,10 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Displays all commands.'),
-    async execute(interaction) {
-        await interaction.reply(`Still in development!`);
-    },
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Get information about a command')
+		.addStringOption(option =>
+			option.setName('commands')
+				.setDescription('commands')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'ban', value: 'bans user of choice' },
+					{ name: 'ping', value: 'replies pong' },
+					{ name: 'verify', value: 'get your roles' },
+				)),
+	async execute(interaction) {
+		const category = interaction.options.getString('commands');
+        ephemeral: true
+	},
 };
+
