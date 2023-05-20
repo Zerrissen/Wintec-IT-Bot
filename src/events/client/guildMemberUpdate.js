c9bb329c5039d59de6381e241138f83e2628ba57
Zerrissen <zerrissen@tutamail.com>
2023-05-20 13:15:00 +0000

    Check for role add Changes to be committed: 	modified:   src/events/client/guildMemberUpdate.js
    Effective changes:
    - Modified guildMemberUpdate so that the logic for checking actually works, and checks the updated cache, not the old one.

--- src/events/client/guildMemberUpdate.js
+++ src/events/client/guildMemberUpdate.js
@@ -3,15 +3,31 @@ const chalk = require('chalk');
 
 module.exports = {
     name: Events.GuildMemberUpdate,
-    async execute(member) {
-        console.log(chalk.cyan(`[Client] ${member.user.username} updated, checking for Student/Alumni..`));
+    async execute(oldMember, member) { //! Yes, oldMember is needed. Don't remove it.
+        console.log(
+            chalk.cyan(
+                `[Client] ${member.user.username} updated, checking for Student/Alumni..`
+            )
+        );
 
-        if (interaction.member.roles.cache.some(
-            (role) => role.name === 'Verified')) {
-            console.log(chalk.green(`[Client] ${member.user.username} verified!`));
+        if (member.roles.cache.some((role) => role.name === 'Verified')) {
+            console.log(
+                chalk.green(`[Client] ${member.user.username} verified!`)
+            );
+        } else if (
+            member.roles.cache.some((role) => role.name === 'Student') ||
+            member.roles.cache.some((role) => role.name === 'Alumni')
+        ) {
+            console.log(
+                chalk.red(`[Client] ${member.user.username} not verified!`)
+            );
+            member.send(
+                "Hi! You've registered yourself as a Wintec student/alumni. Please run the /verify command in the #verify channel to get access to all the other student channels."
+            );
         } else {
-            console.log(chalk.red(`[Client] ${member.user.username} not verified!`));
-            member.send("Hi! You've registered yourself as a Wintec student/alumni. Please run the /verify command in the #verify channel to get access to all the other student channels.")
+            console.log(
+                chalk.green(`[Client] ${member.user.username} is visitor`)
+            );
         }
     },
 };
