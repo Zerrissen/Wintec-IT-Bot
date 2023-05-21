32eabafc0934fa77fd7a2ad90a2633bf67c07698
SkulduggeryDude <131653869+SkulduggeryDude@users.noreply.github.com>
2023-05-21 18:12:24 +1200

    Create balance.js

---
+++ src/schemas/balance.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+const balanceSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    userId: String,
+    balance: { type: Number, default: 0},
+});
+
+module.exports = model('Balance', userSchema, 'balances');
