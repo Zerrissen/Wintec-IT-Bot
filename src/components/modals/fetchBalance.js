7e16679fd3043d04e7e90a15f694cf5ad25b819d
SkulduggeryDude <131653869+SkulduggeryDude@users.noreply.github.com>
2023-05-21 18:44:59 +1200

    Create fetchBalance.js

---
+++ src/components/modals/fetchBalance.js
@@ -0,0 +1,22 @@
+const Balance = require("../../schemas/balance");
+const { Types } = require("mongoose");
+
+module.exports = (client) => {
+  client.fetchBalance = async (userID) => {
+    let storedBalance = await Balance.findOne({
+      userId: userID,
+    });
+    
+    if (!storedBalance) {
+      storedBalance = await new Balance({
+        _id: Types.ObjectID(),
+        userID: userID,
+      });
+      
+      await storedBalance.save().then(async balance => {
+        console.log(`[Balance Created]: UserID: ${balance.userId}`);
+	  }).catch(console.error);
+	  return storedBalance
+	} else return storedBalance
+  };
+};
