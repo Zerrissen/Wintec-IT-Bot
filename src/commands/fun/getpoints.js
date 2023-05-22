ded86a3ce74cedcad83397793f1103bfdfaaa8b0
Zerrissen <zerrissen@tutamail.com>
2023-05-22 03:23:39 +0000

    Profile Embed On branch wizard-bot-making Changes to be committed: 	modified:   src/commands/fun/getpoints.js
    Effective Changes:
    - /points now shows a profile embed of the target user
    - profile embed contains user points, required points to next level, level, and level bar.

--- src/commands/fun/getpoints.js
+++ src/commands/fun/getpoints.js
@@ -2,37 +2,84 @@
 
 // Import necessary modules from Discord.js library
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const Balance = require("../../schemas/balance");
+const Balance = require('../../schemas/balance');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('points')
-		.setDescription('Gives the amount of points a user has!')
-		.addUserOption(option => option.setName('target').setDescription('The users points to see')),
-	async execute(interaction, client) {
-		const selectedUser = interaction.options.getUser('target') || interaction.user;
-		const selectedUserId = selectedUser.id;
+    data: new SlashCommandBuilder()
+        .setName('points')
+        .setDescription('Gives the amount of points a user has!')
+        .addUserOption((option) =>
+            option.setName('target').setDescription('The users points to see')
+        ),
+    async execute(interaction, client) {
+        const selectedUser =
+            interaction.options.getUser('target') || interaction.user;
+        const selectedUserId = selectedUser.id;
 
-		const storedBalance = await Balance.findOne({
-			userId: selectedUserId,
-		});
+        const storedBalance = await Balance.findOne({
+            userId: selectedUserId,
+        });
 
-		if (!storedBalance) return await interaction.reply({
-			content: `${selectedUser.tag}, doesnt have a balance.`,
-			ephemeral: true
-		});
-		else {
-			const embed = new EmbedBuilder()
-				.setTitle(`${selectedUSer.username}'s Balance: `)
-				.addFields([
-					{
-						name: `$${storedBalance.balance}`,
-					}
-				])
-			await interaction.reply({
-				embeds: [embed],
-				ephemeral: true,
-			});
-		}
-	},
+        if (!storedBalance)
+            return await interaction.reply({
+                content: `@${selectedUser.username}, doesnt have a balance.`,
+                ephemeral: true,
+            });
+        else {
+            const userPoints = storedBalance.balance;
+
+            const userLevel = String(
+                Math.floor(Math.log2(userPoints / 10 + 1)) + 1
+            );
+            const totalPointsToNextLevel =
+                10 * (Math.pow(2, parseInt(userLevel)) - 1);
+            const reqXp = String(totalPointsToNextLevel - userPoints);
+            const filledChar = '▰';
+            const emptyChar = '▱';
+
+            // Calculate the number of filled and empty characters
+            const filledCount = Math.floor(
+                (userPoints / totalPointsToNextLevel) * 25
+            );
+            const emptyCount = 25 - filledCount;
+
+            // Generate the level bar string
+            const levelBar =
+                filledChar.repeat(filledCount) + emptyChar.repeat(emptyCount);
+
+            const embed = new EmbedBuilder()
+                .setTitle(`${selectedUser.username}'s Profile`)
+                .setColor(0x02621c)
+                .setThumbnail(`${selectedUser.displayAvatarURL()}`)
+                .setDescription(
+                    `This profile contains information about the user and their wizard points!`
+                )
+                .addFields(
+                    {
+                        name: 'Wizard Points',
+                        value: `User has ${storedBalance.balance} points!`,
+                        inline: true,
+                    },
+                    {
+                        name: 'Tech Wizard Level',
+                        value: userLevel,
+                        inline: true,
+                    },
+                    {
+                        name: 'Required Points To Next Level',
+                        value: reqXp,
+                        inline: false,
+                    },
+                    {
+                        name: 'Level Bar',
+                        value: levelBar,
+                        inline: false,
+                    }
+                )
+                .setTimestamp();
+            await interaction.reply({
+                embeds: [embed],
+            });
+        }
+    },
 };
