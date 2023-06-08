4547d0b164eb02f1bd6d6c546563857fce413530
Zerrissen <zerrissen@tutamail.com>
2023-06-08 21:13:13 +0000

    API Errors handled On branch handle-api-errors Changes to be committed: 	modified:   src/commands/moderation/ban.js 	modified:   src/commands/moderation/kick.js 	modified:   src/commands/moderation/timeout.js 	modified:   src/commands/moderation/unban.js 	modified:   src/components/modals/verify-email.js 	modified:   src/events/client/guildMemberAdd.js 	modified:   src/events/client/guildMemberUpdate.js
    Effective changes:
    - Found multiple bugs that had the potential to cause the bot when an unhandled API error was fired
    - Resolved by added catch() blocks to each member.send() method found.

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -1,11 +1,15 @@
 const { Events } = require('discord.js');
 const chalk = require('chalk');
-const User = require('../../schemas/user')
+const User = require('../../schemas/user');
 
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member) {
-        console.log(chalk.cyan(`[Client] Checking if ${member.user.username} is verified...`));
+        console.log(
+            chalk.cyan(
+                `[Client] Checking if ${member.user.username} is verified...`
+            )
+        );
 
         const filter = { userId: member.user.id };
         const update = {
@@ -23,12 +27,26 @@ module.exports = {
                 (role) => role.name === 'Verified'
             );
             interaction.member.roles.add(role);
-            console.log(chalk.green(`[Client] @${member.user.username} verified!`));
+            console.log(
+                chalk.green(`[Client] @${member.user.username} verified!`)
+            );
 
-            member.send("Howdy! It seems you've rejoined the **Wintec IT Student** server. Welcome back! You don't have to verify again, just select the Student or Alumni role! Also, please don't forget to change your server nickname to your first name!")
-        }
-        else {
-            console.log(chalk.red(`[Client] @${member.user.username} not verified!`));
+            member
+                .send(
+                    "Howdy! It seems you've rejoined the **Wintec IT Student** server. Welcome back! You don't have to verify again, just select the Student or Alumni role! Also, please don't forget to change your server nickname to your first name!"
+                )
+                // Use the following catch for every time we call member.send. Unfortunately no other way around this. See https://discordjs.guide/popular-topics/errors.html#cannot-send-messages-to-this-user
+                .catch((error) => {
+                    console.log(
+                        chalk.red(
+                            `[API] ${member.user.username} cannot be messaged. Not DMing! Error code: ${error.code}`
+                        )
+                    );
+                });
+        } else {
+            console.log(
+                chalk.red(`[Client] @${member.user.username} not verified!`)
+            );
         }
     },
 };
