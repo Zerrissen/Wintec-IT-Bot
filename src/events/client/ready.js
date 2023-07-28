9adb6f4cb298e5b7af2155fcbd9df5e19b309d87
Zerrissen <zerrissen@tutamail.com>
2023-07-28 16:02:48 +1200

    Fix leaderboard, add caching Changes to be committed: 	modified:   src/commands/fun/leaderboard.js 	modified:   src/events/client/ready.js
    Effective changes:
        - Fixed issue where bot would crash while searching for user that isn't in guild due to difference between mongo db and discord guild
        - Bot now caches all members on startup

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -6,5 +6,12 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(chalk.green(`[Client] Logged in as @${client.user.username}`));
+
+        const guilds = client.guilds.cache.map(guild => guild.id);
+        for (const id of guilds) {
+            const guild = await client.guilds.fetch(id);
+            const members = await guild.members.fetch();
+            console.log(chalk.green(`[Cache] Cache for guild ${id} built!`));
+        }
     },
 };
