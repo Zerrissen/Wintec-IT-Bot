c37b71221cdf84bbcc57cc519b080971dd90c42c
Thomas Karam <131653869+SkulduggeryDude@users.noreply.github.com>
2023-08-02 19:15:28 +1200

    Commented version

--- src/commands/fun/leaderboard.js
+++ src/commands/fun/leaderboard.js
@@ -16,99 +16,37 @@ module.exports = {
                 const embed = new EmbedBuilder()
                     .setColor(0x0f4a00)
                     .setTitle("Wizard Point Leaderboard");
-                let position = 0;
-                let counter = 0;
+                let position = 0; //Whenever a valid user is found, position is +1 and used in embed
+                let counter = 0; //Whenever any user is found, the counter for the loop goes up by one
 
-              
-                while (counter <= 10 && counter < leaderboard.length) {
-                    let record = leaderboard[counter]
-                    // console.log("leadercounter" + leaderboard[counter])
-                    // console.log("record.userId" + record.userId + "\n")
+
+                //counter < leaderboard.length prevents reading past the number of entrees that exist
+                //position <= 10 caps the leaderboard at 10 spots
+
+                while (position <= 10 && counter < leaderboard.length) { //counter < leaderboard.length prevents reading past the number of entrees that exist
+                    let record = leaderboard[counter] //The users information inside the database is stored in variable 'record'
                     const member = await interaction.guild.members
-                        .fetch(record.userId)
+                        .fetch(record.userId) //.fetch grabs the information userId stored inside record to use in output
                         .catch((error) => {
                             console.log(
                                 chalk.red(
                                     `[API] Cannot fetch user ID ${record.userID}! No longer in server. Error code: ${error.code}`
-                                    
                                 )
                             );
-                            
                         });
-
-                    const memberName = member?.user?.username || 'Unknown User';
-                    if (memberName != 'Unknown User') {
-                        position++;
+                    
+                    
+                    const memberName = member?.user?.username || 'Unknown User'; //Retrives user information
+                    if (memberName != 'Unknown User') { //If the user is a vaild current user, they get added to the leaderboard
+                        position++; //Makes their position one after the last valid user
                         embed.addFields({
                             name: `#${String(position)} @${memberName}`,
                             value: `${String(record.balance)} points`,
-                        });
-                        
+                        }); 
                     }
-                    counter++;
+                    counter++; //Adds 1 to counter so the loop will go to the next user
                 }
-
-
-                interaction.reply({ embeds: [embed] });
-                
+                interaction.reply({ embeds: [embed] }); //Sends the message to the server
             });
     },
 };
-
-// // Import necessary modules from Discord.js library
-// const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-// const chalk = require("chalk");
-// const Balance = require("../../schemas/balance"); // Import the Balance schema for accessing the database
-
-// module.exports = {
-//     // Create a new slash command with the name "leaderboard" and description "Gives the amount of points a user has!"
-//     data: new SlashCommandBuilder()
-//         .setName("leaderboard")
-//         .setDescription("Gives the amount of points a user has!"),
-
-//     // The execute function is called when the "leaderboard" command is triggered
-//     async execute(interaction) {
-//         // Fetch the top 10 records from the Balance collection, sorted by balance in descending order
-//         Balance.find()
-//             .sort({ balance: -1 })
-//             .limit(10)
-//             .then(async (leaderboard) => {
-//                 // Create a new embed message to display the leaderboard
-//                 const embed = new EmbedBuilder()
-//                     .setColor(0x0f4a00)
-//                     .setTitle("Wizard Point Leaderboard");
-
-//                 let position = 1; // Initialize a position counter for ranking
-
-//                 // Loop through the leaderboard records and fetch the associated member from the guild
-//                 for (record of leaderboard) {
-//                     const member = await interaction.guild.members
-//                         .fetch(record.userId)
-//                         .catch((error) => {
-//                             // If the user is no longer in the server, handle the error
-//                             console.log(
-//                                 chalk.red(
-//                                     `[API] Cannot fetch user ID ${record.userID}! No longer in server. Error code: ${error.code}`
-//                                 )
-//                             );
-//                         });
-
-//                     // Make sure user is still in the server and get the member's username
-//                     const memberName = member?.user?.username || 'Unknown User';
-
-//                     // Check if the member is not an "Unknown User"
-//                     if (memberName != 'Unknown User') {
-//                         // Add the member's username and balance to the embed message
-//                         embed.addFields({
-//                             name: `#${String(position)} @${memberName}`,
-//                             value: `${String(record.balance)} points`,
-//                         });
-//                         position++; // Increment the position for the next member
-//                     }
-//                 }
-
-//                 // Send the embed message containing the leaderboard to the interaction channel
-//                 interaction.reply({ embeds: [embed] });
-//             });
-//     },
-// };
