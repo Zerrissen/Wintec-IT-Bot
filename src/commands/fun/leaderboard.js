7a9ffb4032199a360baf99e3ec6a49063c53211d
SkulduggeryDude <thomas.k747400@gmail.com>
2023-08-02 15:05:07 +1200

    changing for to while loop

--- src/commands/fun/leaderboard.js
+++ src/commands/fun/leaderboard.js
@@ -7,18 +7,19 @@ const Balance = require("../../schemas/balance");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("leaderboard")
-        .setDescription("Gives the amount of points a user has!"),
+        .setDescription("Displays the top 10 users with wizard points!"),
     async execute(interaction) {
         Balance.find()
             .sort({ balance: -1 })
-            .limit(10)
+            .limit(20)
             .then(async (leaderboard) => {
                 const embed = new EmbedBuilder()
                     .setColor(0x0f4a00)
                     .setTitle("Wizard Point Leaderboard");
-                let position = 1;
+                let position = 0;
+                let counter = 0;
 
-                for (record of leaderboard) {
+                while (counter <= 10) {
                     const member = await interaction.guild.members
                         .fetch(record.userId)
                         .catch((error) => {
@@ -28,16 +29,39 @@ module.exports = {
                                 )
                             );
                         });
-                    // make sure user is still in server
+
                     const memberName = member?.user?.username || 'Unknown User';
                     if (memberName != 'Unknown User') {
                         embed.addFields({
                             name: `#${String(position)} @${memberName}`,
                             value: `${String(record.balance)} points`,
                         });
-                        position++;
+                        counter++;
                     }
                 }
+
+
+
+                // for (record of leaderboard) {
+                //     const member = await interaction.guild.members
+                //         .fetch(record.userId)
+                //         .catch((error) => {
+                //             console.log(
+                //                 chalk.red(
+                //                     `[API] Cannot fetch user ID ${record.userID}! No longer in server. Error code: ${error.code}`
+                //                 )
+                //             );
+                //         });
+                //     // make sure user is still in server
+                //     const memberName = member?.user?.username || 'Unknown User';
+                //     if (memberName != 'Unknown User') {
+                //         embed.addFields({
+                //             name: `#${String(position)} @${memberName}`,
+                //             value: `${String(record.balance)} points`,
+                //         });
+                //         position++;
+                //     }
+                // }
 
                 interaction.reply({ embeds: [embed] });
             });
