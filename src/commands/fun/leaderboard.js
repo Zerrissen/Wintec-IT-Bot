aa5f3915c8a8689543a6b3603e7baa689e77f903
Thomas Karam <131653869+SkulduggeryDude@users.noreply.github.com>
2023-08-02 17:57:19 +1200

    test

--- src/commands/fun/leaderboard.js
+++ src/commands/fun/leaderboard.js
@@ -19,32 +19,11 @@ module.exports = {
                 let position = 0;
                 let counter = 0;
 
-                while (counter <= 10) {
-                    const member = await interaction.guild.members
-                        .fetch(leaderboard.record.userId)
-                        .catch((error) => {
-                            console.log(
-                                chalk.red(
-                                    `[API] Cannot fetch user ID ${record.userID}! No longer in server. Error code: ${error.code}`
-                                )
-                            );
-                        });
+                console.log(leaderboard);
 
-                    const memberName = member?.user?.username || 'Unknown User';
-                    if (memberName != 'Unknown User') {
-                        embed.addFields({
-                            name: `#${String(position)} @${memberName}`,
-                            value: `${String(record.balance)} points`,
-                        });
-                        counter++;
-                    }
-                }
-
-
-
-                // for (record of leaderboard) {
+                // while (counter <= 10) {
                 //     const member = await interaction.guild.members
-                //         .fetch(record.userId)
+                //         .fetch(leaderboard.userId)
                 //         .catch((error) => {
                 //             console.log(
                 //                 chalk.red(
@@ -52,18 +31,78 @@ module.exports = {
                 //                 )
                 //             );
                 //         });
-                //     // make sure user is still in server
+
                 //     const memberName = member?.user?.username || 'Unknown User';
                 //     if (memberName != 'Unknown User') {
                 //         embed.addFields({
                 //             name: `#${String(position)} @${memberName}`,
                 //             value: `${String(record.balance)} points`,
                 //         });
-                //         position++;
+                //         counter++;
                 //     }
                 // }
 
-                interaction.reply({ embeds: [embed] });
+
+                // interaction.reply({ embeds: [embed] });
+                
             });
     },
 };
+
+// // Import necessary modules from Discord.js library
+// const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+// const chalk = require("chalk");
+// const Balance = require("../../schemas/balance"); // Import the Balance schema for accessing the database
+
+// module.exports = {
+//     // Create a new slash command with the name "leaderboard" and description "Gives the amount of points a user has!"
+//     data: new SlashCommandBuilder()
+//         .setName("leaderboard")
+//         .setDescription("Gives the amount of points a user has!"),
+
+//     // The execute function is called when the "leaderboard" command is triggered
+//     async execute(interaction) {
+//         // Fetch the top 10 records from the Balance collection, sorted by balance in descending order
+//         Balance.find()
+//             .sort({ balance: -1 })
+//             .limit(10)
+//             .then(async (leaderboard) => {
+//                 // Create a new embed message to display the leaderboard
+//                 const embed = new EmbedBuilder()
+//                     .setColor(0x0f4a00)
+//                     .setTitle("Wizard Point Leaderboard");
+
+//                 let position = 1; // Initialize a position counter for ranking
+
+//                 // Loop through the leaderboard records and fetch the associated member from the guild
+//                 for (record of leaderboard) {
+//                     const member = await interaction.guild.members
+//                         .fetch(record.userId)
+//                         .catch((error) => {
+//                             // If the user is no longer in the server, handle the error
+//                             console.log(
+//                                 chalk.red(
+//                                     `[API] Cannot fetch user ID ${record.userID}! No longer in server. Error code: ${error.code}`
+//                                 )
+//                             );
+//                         });
+
+//                     // Make sure user is still in the server and get the member's username
+//                     const memberName = member?.user?.username || 'Unknown User';
+
+//                     // Check if the member is not an "Unknown User"
+//                     if (memberName != 'Unknown User') {
+//                         // Add the member's username and balance to the embed message
+//                         embed.addFields({
+//                             name: `#${String(position)} @${memberName}`,
+//                             value: `${String(record.balance)} points`,
+//                         });
+//                         position++; // Increment the position for the next member
+//                     }
+//                 }
+
+//                 // Send the embed message containing the leaderboard to the interaction channel
+//                 interaction.reply({ embeds: [embed] });
+//             });
+//     },
+// };
