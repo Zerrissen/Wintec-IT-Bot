0be06a89cc6f303b74e49567ca4b5f0066454774
Zerrissen <zerrissen@tutamail.com>
2023-10-19 20:56:12 +1300

    Add axios package, add /wiki command Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/commands/utility/wiki.js

---
+++ src/commands/utility/wiki.js
@@ -0,0 +1,63 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("wiki")
+        .setDescription(
+            "Search for a Wikipedia page and return its introduction"
+        )
+        .addStringOption((option) =>
+            option
+                .setName("query")
+                .setDescription("The search query")
+                .setRequired(true)
+        ),
+    // Time to do our wikipedia query using the wikipedia API
+    // We want to use the search endpoint to allow for more flexibility in the search query
+    async execute(interaction) {
+        const query = interaction.options.getString("query");
+
+        const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(
+            query
+        )}&utf8=&format=json`;
+        const searchResponse = await axios.get(searchUrl);
+        const searchResults = searchResponse.data.query.search;
+
+        if (searchResults.length === 0) {
+            await interaction.reply("No results found for the given query.");
+            return;
+        }
+
+        const title = searchResults[0].title;
+
+        // Now we can parse the search result title to get the page summary with a secondary API request
+        // We can also grab the wikipedia page thumbnail
+        const summaryUrl = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(
+            title
+        )}`;
+        try {
+            const summaryResponse = await axios.get(summaryUrl);
+            const { extract, content_urls, thumbnail, originalimage } =
+                summaryResponse.data;
+
+            const embed = new EmbedBuilder()
+                .setColor(0x0f4a00)
+                .setTitle(title)
+                .setURL(content_urls.desktop.page)
+                .setDescription(extract);
+
+            // Some embeds don't have thumbnails
+            if (thumbnail) {
+                embed.setImage(thumbnail.source);
+            }
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply(
+                "An error occurred while searching for the Wikipedia page."
+            );
+        }
+    },
+};
